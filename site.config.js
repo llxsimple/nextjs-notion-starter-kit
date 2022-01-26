0e18463f5e86e8c587d3f658d8a4e0d5dc4c968e
Serenade <510374040@qq.com>
2022-01-26 16:11:16 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'decce6bdcdd740739e1cd921bc7fa18e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: '凌晨',
   domain: 'transitivebullsh.it',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '凌晨',
+  socialImageTitle: 'https://sparkling-napkin-318.notion.site/decce6bdcdd740739e1cd921bc7fa18e?v=b2f8c2a412ee419291bcf75e570b08a0',
+  socialImageSubtitle: '凌晨! 👋',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
