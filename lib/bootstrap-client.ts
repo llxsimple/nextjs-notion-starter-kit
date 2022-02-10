2f520a6bd5572515951a7d477877fbea340d2a88
Serenade <510374040@qq.com>
2022-02-10 11:36:56 +0800

    Update bootstrap-client.ts

--- lib/bootstrap-client.ts
+++ lib/bootstrap-client.ts
@@ -1,3 +1,3 @@
 export function bootstrap() {
-　　console.log("%c ", "background: url(https://i.postimg.cc/Ssy6MqH7/ggds.png) no-repeat center;padding-left:500px;padding-bottom: 242px;")
+　　console.log("666666", "background: url(https://i.postimg.cc/Ssy6MqH7/ggds.png) no-repeat center;padding-left:500px;padding-bottom: 242px;")
 }
